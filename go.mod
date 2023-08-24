fd93c3ab7161d2d811f2bc67a386e11eb285ee1e
wujinzbr <wujinzbr@163.com>
2023-08-24 16:43:17 +0800

    isRequestAllowed 挪位置

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.12
 require (
 	github.com/elazarl/go-bindata-assetfs v1.0.0
 	github.com/json-iterator/go v1.1.10
-	github.com/lxn/win v0.0.0-20191128105842-2da648fda5b4
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e
 	github.com/markbates/pkger v0.17.0
 	github.com/modern-go/reflect2 v1.0.1
 	github.com/mushroomsir/mimetypes v0.0.0-20171113080813-3b41529ef757
--- webview.go
+++ webview.go
@@ -5,6 +5,7 @@ import "C"
 import (
 	"os"
 	"reflect"
+	"strings"
 	"unsafe"
 
 	"github.com/lxn/win"
@@ -391,4 +392,28 @@ func (view *WebView) SetUrlEndHandler(value interface{}) {
 // 添加urlEndHandlerMimeTypes处理类型
 func (view *WebView) AddUrlEndHandlerMimeTypes(args ...string) {
 	view.urlEndHandlerMimeTypes = append(view.urlEndHandlerMimeTypes, args...)
+}
+
+// 黑白名单检查 请求发出之前检查 若未通过检测则不会发送请求
+func (view *WebView) isRequestAllowed(url string) bool {
+	var allow bool = false
+	if len(view.whitelist) > 0 {
+		// 白名单模式下 不匹配任意一白名单则禁止访问
+		for _, v := range view.whitelist {
+			if strings.Contains(url, v) {
+				allow = true
+			}
+		}
+		if !allow {
+			return allow
+		}
+	}
+	allow = true
+	// 匹配任意一黑名单则禁止访问
+	for _, v := range view.blacklist {
+		if strings.Contains(url, v) {
+			allow = false
+		}
+	}
+	return allow
 }
